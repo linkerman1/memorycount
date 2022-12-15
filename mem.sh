6a14e28d00b6aaf05496b3b39e9902999deac71f
Alex Prohorov <oleksiy0007@gmail.com>
2022-12-15 23:16:30 +0100

    Update mem.sh

--- mem.sh
+++ mem.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 find /root/wallie/qemu-server2  /root/wallie/qemu-server  -type f -name "*.conf" -exec grep -E -w -R 'memory' {} \; >memory.txt
+awk '{ sum += $2 } END { print "\n My RAM is full at the moment:", sum}' memory.txt > memory.tmp && mv memory.tmp memory.txt
